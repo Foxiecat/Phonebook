fad68f8a7b332f5c1316109ebe28d1ad5dd6e5f2
Danica Ellefsen Kvilhaug <65165280+Foxiecat@users.noreply.github.com>
2024-09-23 16:03:32 +0200

    Created
    Created ContactController.cs

---
+++ Phonebook/Features/Contacts/ContactController.cs
@@ -0,0 +1,22 @@
+using System.Runtime.InteropServices.JavaScript;
+using static Phonebook.Features.Utilities;
+using Bogus;
+
+namespace Phonebook.Features.Contacts;
+
+public class ContactController
+{
+    public static string Display(Contact contact)
+    {
+    }
+
+    public Contact[] Search(string criteria)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Contact[] Sort(string criterion)
+    {
+        throw new NotImplementedException();
+    }
+}
