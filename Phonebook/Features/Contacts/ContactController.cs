ad19ef0dc6755a07d9f81bdcdbcf92a8b24a5869
Danica Ellefsen Kvilhaug <65165280+Foxiecat@users.noreply.github.com>
2024-09-23 16:04:27 +0200

    Created Generate()
    Created Generate() function

--- Phonebook/Features/Contacts/ContactController.cs
+++ Phonebook/Features/Contacts/ContactController.cs
@@ -19,4 +19,38 @@ public class ContactController
     {
         throw new NotImplementedException();
     }
+    
+    /// <summary>
+    /// Generates 100 fake Norwegian contacts.
+    /// </summary>
+    /// <returns>A new list of fake contacts</returns>
+    public static Contact[] Generate()
+    {
+        // Sets the Faker locale to Norwegian
+        Faker faker = new("nb_NO");
+        
+        Contact[] fakeContacts = new Contact[100];
+        
+        for (int index = 0; index < 100; index++)
+        {
+            Contact contact = new()
+            {
+                FirstName = faker.Name.FirstName(),
+                LastName = faker.Name.LastName(),
+                MobileNumber = faker.Random.Int(40000000, 99999999),
+                Birthday = faker.Date.Between(DateTime.Now.AddYears(-80), DateTime.Now.AddYears(-18)).ToString("dd/MM/yyyy"),
+                Address = faker.Address.StreetAddress() + ", " + faker.Address.City()
+            };
+            
+            fakeContacts[index] = contact;
+        }
+        
+        // Serialize the fakeContacts array to a JSON string
+        string jsonString = Serialize(fakeContacts);
+        
+        // Writes a JSON file from the jsonString
+        File.WriteAllText("../../../Logs/contacts.json", jsonString);
+        
+        return fakeContacts;
+    }
 }
